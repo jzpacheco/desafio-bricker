1dfd14754674a986234de25bb8e1e3fb77ab2f05
jzpacheco <jeffersonpacheco@hotmail.com>
2024-03-02 23:55:06 -0300

    feat: text extracted from img inside dpf

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 import fitz
+import boto3
+
+textract = boto3.client('textract')
+
+
 
 def extract_image_from_pdf(pdf_path):
-    text = ""
+ 
     with fitz.open(pdf_path) as doc:
         for page_num, page in enumerate(doc):
             images = page.get_images(full=True)
-            print(images)
-
             for i, img in enumerate(images):
                 xref= img[0]
                 image_bytes = doc.extract_image(xref)["image"]
-                print(image_bytes)
+                text = extract_text_from_image(image_bytes)
+                print('LA: '+ text)
+                return text
+
+                
 
 
 def extract_text_from_image(image_bytes):
+    text = ''
+    response = textract.detect_document_text(Document={'Bytes': image_bytes})
+    
+    for item in response['Blocks']:
+        if item['BlockType' ] == 'LINE':
+            text += item['Text'] + '\n'
+    print('AQUI: '+ text)
+    return text
+    
     
 
 extract_image_from_pdf('C:/Users/jeffe/Downloads/matricula-6.pdf')
