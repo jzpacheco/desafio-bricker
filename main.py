b2659603d27b9fac186873e3afe75b0a571167f5
jzpacheco <jeffersonpacheco@hotmail.com>
2024-03-03 22:48:33 -0300

    feat: extracting relevant info using llm

--- main.py
+++ main.py
@@ -1,12 +1,51 @@
 import fitz
 import boto3
 
+from langchain_openai import ChatOpenAI
+from langchain.chains import create_extraction_chain, create_structured_output_runnable
+
 textract = boto3.client('textract')
+openai_key = "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"
 
+structured_schema = {
+    "type": "object",
+    "properties": {
+        "expedition_date": {"type": "string", "description": "Data de emissão do certificado, DD/MM/YYYY"},
+        "responsible_registry": {"type": "string"},
+        "registration_number": {"type": "string"},
+        "property_type": {"type": "string"},
+        "batch": {"type": "string"},
+        "block": {"type": "string"},
+        "street": {"type": "string"},
+        "street_2": {"type": "string"},
+        "property_number": {"type": "string"},
+        "property_number_2": {"type": "string"},
+        "address_complement": {"type": "string"},
+        "neighborhood": {"type": "string"},
+        "city": {"type": "string"},
+        "state": {"type": "string"},
+        "property_footage": {"type": "string"},
+        "owners_quantity": {"type": "string"},
+        "owner_1": {"type": "string"},
+        "owner_2": {"type": "string"},
+        "document_owner_1": {"type": "string", "description":"CPF / CNPJ"},
+        "document_owner_2": {"type": "string"},
+        "mortgage": {"type": "string"},
+        "mortgage_owner_name": {"type": "string"},
+        "mortgage_owner_document": {"type": "string"},
+        "fiduciary_alienation": {"type": "string"},
+        "fiduciary_alienation_owner_name": {"type": "string"},
+        "fiduciary_alienation_owner_document": {"type": "string"},
+        "garnishment": {"type": "string"}
+    }
+}
 
+llm = ChatOpenAI(temperature=0.7, model="gpt-3.5-turbo", openai_api_key=openai_key)
+
+extraction_chain = create_structured_output_runnable(structured_schema, llm)
 
 def extract_image_from_pdf(pdf_path):
- 
+  
     with fitz.open(pdf_path) as doc:
         for page_num, page in enumerate(doc):
             images = page.get_images(full=True)
@@ -32,4 +71,7 @@ def extract_text_from_image(image_bytes):
     
     
 
-extract_image_from_pdf('C:/Users/jeffe/Downloads/matricula-6.pdf')
+doc = extract_image_from_pdf('C:/Users/jeffe/Downloads/matricula-6.pdf')
+
+response = extraction_chain.invoke(doc)
+print(response)
